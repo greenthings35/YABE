85f1a8390b5a9f66433391ae4e60fb5f05492f1b
Deinebuda1911 <1911dado@gmail.com>
2025-12-08 17:24:32 +0100

    code for enemy chase

--- YABE/Assets/Enemy/Enemyscript.cs
+++ YABE/Assets/Enemy/Enemyscript.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    public GameObject Player;
+    public float speed;
+
+    private float distance;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,9 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        distance = Vector2.Distance(transform.position, Player.transform.position); // Calculates distance between enemy and player
+        Vector2 direction = Player.transform.position - transform.position; // Gets direction vector from enemy to player  
+
+        transform.position = Vector2.MoveTowards(this.transform.position, Player.transform.position, speed * Time.deltaTime); // Moves enemy towards player at a set speed
     }
 }
